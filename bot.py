bd64130232990c11bae7d77a44e59f8b23a69c55
goelakash <goelakash93@gmail.com>
2016-05-07 14:36:36 +0530

    streaming api

--- bot.py
+++ bot.py
@@ -9,25 +9,55 @@ consumer_secret = "HxwaAwLisDBiZqBTqLt4AVDHMcba0ZRRyJobwjZ2ubMdgRhVXo"
 access_token_key = "2450454258-64KnS3q2NcT2Ti3gjQa8UDq0rMIXVRfnXQRWB4U"
 access_token_secret = "KRG6K0y6KaJr56qqSADwSdmUSOuxbyUvKV8RRR8oQd9B5"
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.secure = True
-auth.set_access_token(access_token_key, access_token_secret)
+class Listener( StreamListener ):
 
-api = API(auth)
+    def __init__( self ):
+        self.tweetCount = 0
 
-# If the authentication was successful, you should
-# see the name of the account print out
-print("List of all received messages:\n\n")
-print("receiver name: ",api.me().name)
-print("receiver handle: ",api.me().screen_name)
+    def on_connect( self ):
+        print("Connection established!!")
 
-messages = api.direct_messages()
+    def on_disconnect( self, notice ):
+        print("Connection lost!! : ", notice)
 
-for i in range(len(messages)):
-    print("\n\nMessage: ",i)
-    print("text: ",messages[i].text)
-    print("sender name: ",messages[i].sender.name)
-    print("sender hande: ",messages[i].sender_screen_name)
-    # print("receiver handle: ",messages[i].recipient_screen_name)
-    print("sent at: ",messages[i].created_at.strftime("%d-%m-%Y"))
+    def on_data( self, status ):
+        print("Entered on_data()")
+        print(status, flush = True)
+        return True
+
+    def on_direct_message( self, status ):
+        print("Entered on_direct_message()")
+        try:
+            print(status, flush = True)
+            return True
+        except BaseException as e:
+            print("Failed on_direct_message()", str(e))
+
+    def on_error( self, status ):
+        print(status)
+
+
+
+def main():
+
+    try:
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.secure = True
+        auth.set_access_token(access_token_key, access_token_secret)
+
+        api = API(auth)
+
+        # If the authentication was successful, you should
+        # see the name of the account print out
+        print(api.me().name)
+
+        stream = Stream(auth, Listener())
+
+        stream.userstream()
+
+    except BaseException as e:
+        print("Error in main()", e)
+
+if __name__ == '__main__':
+    main()
 
