cc5d037282ef909309dbb32b57239c6391a45a40
goelakash <goelakash93@gmail.com>
2016-05-08 02:25:32 +0530

    API stage 1 completed

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from tweepy import API
 
 from tweepy.streaming import StreamListener
 import json
+import re
 
 consumer_key = "agjequxG75iT0dHxn7jCd1uLW"
 consumer_secret = "HxwaAwLisDBiZqBTqLt4AVDHMcba0ZRRyJobwjZ2ubMdgRhVXo"
@@ -23,14 +24,41 @@ acc_id = ['572dbccafd3e5d6025180a10', #0 balance
 '572dbcc9fd3e5d6025180a0f'] #14389.1
 
 handle_to_customerid= {
-
     '_goelakash' : "572dbcc9fd3e5d6025180a0e"
 }
+
+
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
 class Listener( StreamListener ):
 
     def __init__( self ):
         self.mesgCount = 0
+        self.conn = httplib.HTTPSConnection('bluebank.azure-api.net')
+
+
+    def str_list_to_json( self, response):
+        response = response.split("}")
+        response = response[:-1]
+        for i in range(len(response)):
+            response[i] = response[i][1:]   #[{...}, {...}, {...}]
+            response[i] = response[i]+'}'
+        for i in range(len(response)):
+            response[i] = json.loads(response[i])
+            # print(response[i])
+        return response
+
+
+    def get_accounts(self, cust_id):
+        conn = self.conn
+        conn.request("GET", "/api/v0.6.3/customers/"+cust_id+"/accounts", "", headers)
+        data = str_list_to_json(conn.getresponse().read())
+
+        accounts = []
+        for x in data:
+            accounts.append(x)
+        return accounts
+
 
     def on_status(self, status):
         print("Status text!!")
@@ -41,6 +69,8 @@ class Listener( StreamListener ):
 
     def on_disconnect( self, notice ):
         print("Connection lost!! : ", notice)
+
+
 
     def on_data( self, status ):
         print("Entered on_data()")
@@ -56,100 +86,145 @@ class Listener( StreamListener ):
             print("Sent at: ",mesg["created_at"])
 
             text = text.lower().split(" ")
-            conn = httplib.HTTPSConnection('bluebank.azure-api.net')
 
             if text[0] == "help":
                 # list all the commands
 
 
-            elif text[0] == "accounts":
+            '''
+            1. Account info
+            '''
+            elif text[0] == "account" and text[1] == "info":
                 try:
-                    if text[1] == "info":
-                        conn.request("GET", "/api/v0.6.3/accounts/572dbcc9fd3e5d6025180a0f","",headers)
-                        response = conn.getresponse()
-                        data = response.read()
-                        print(data)
+                    accounts = self.get_accounts(customer_id)
+                    for k,acc in zip(range(len(data)),data):
+                        print(k+1,".")
+                        print("Acc no: ",acc["accountNumber"])
+                        print("Acc type: ",acc["accountType"])
+                        print("Acc balance: ",acc["accountBalance"] + " "+ acc["accountCurrency"])
+                except e:
+                    print("Exception: ",e)
 
-                    elif text[1] == "update":
-                        body = {
-                            text[2]:text[3]
-                        }
-                        if text[2]=="accountFriendlyName":
-                            conn.request("PATCH", "/api/v0.6.3/accounts/572dbcc9fd3e5d6025180a0f",body,headers)
-                            response = conn.getresponse()
-                            data = response.read()
-                            print(data)
 
+            '''
+            2. Transaction history
+            '''
+            elif text[0] == "transaction" and text[1] == "history":
+                limit = 5
+                try:
+                    if text[2]:
+                        if text[2].isdigit():
+                            limit = int(text[2])
                         else:
-                            print("Cannot update this attribute.")
+                            raise ValueError('Illegal value!')
 
-                    elif text[1] == "payments":
-                        conn.request("GET", "/api/v0.6.3/accounts/572dbcc9fd3e5d6025180a0f/payments","",headers)
+                    accounts = self.get_accounts(customer_id)
+                    print("Description | Amount | DateTime | Balance")
+
+                    for acc in accounts:
+                        print("for account no: ",acc["accountNumber"],")")
+                        self.conn.request("GET", "/api/v0.6.3/accounts/"+acc["accountNumber"]+"/transactions?limit="+limit,"",headers)
                         response = conn.getresponse()
-                        data = response.read()
-                        print(data)
+                        data = str_list_to_json(response.read())
 
-                    elif text[1] == "transactions":
-                        conn.request("GET", "/api/v0.6.3/accounts/572dbcc9fd3e5d6025180a0f/transactions","",headers)
-                        response = conn.getresponse()
-                        data = response.read()
-                        print(data)
+                        for i in range(len(data)):
+                            print(i+1,". ",data[i]["transactionDescription"]," | ",data[i]["transactionAmount"]," | ",
+                                data[i]["transactionDateTime"]," | ",data[i]["accountBalance"])
 
-                    else:
-                        print("Illegal operation!\n")
 
                 except e:
                     print("An exception happened: ",e)
 
-            elif text[0] == "customers":
+            '''
+            3. User info
+            '''
+            elif text[0] == "my" and text[1]=="info":
                 try:
-                    if text[1] == "all":
-                        conn.request("GET", "/api/v0.6.3/customers","",headers)
+                    if text[2] == "display":
+                        self.conn.request("GET", "/api/v0.6.3/customers/"+customer_id,"",headers)
                         response = conn.getresponse()
-                        data = response.read()
-                        print(data)
+                        data = json.loads(response().read())[0]
+                        for x in data:
+                            print(x+": "+data[x])
 
-                    elif text[1] == "info":
-                        conn.request("GET", "/api/v0.6.3/customers/"+customer_id,"",headers)
-                        response = conn.getresponse()
-                        data = response.read()
-                        print(data)
+                    elif text[2] == "update":
+                        if text[3] and text[4]:
+                            if text[3] == "email":
+                                if not EMAIL_REGEX.match(text[4]):
+                                    raise ValueError('Invalid email address!')
+                                body = {
+                                    'emailID':text[4]
+                                }
+                                self.conn.request("PATCH","/api/v0.6.3/customers/"+customer_id,json.dumps(body), headers)
 
-                    elif text[1] == "accounts":
-                        conn.request("GET", "/api/v0.6.3/customers/"+customer_id+"/accounts","",headers)
-                        response = conn.getresponse()
-                        data = response.read()
-                        print(data)
+                            elif text[3] == "mobile":
+                                if not text[4].isdigit() or not len(text[4])==10:
+                                    raise ValueError('Invalid mobile number!')
+                                body = {
+                                    'mobilePhone':text[4]
+                                }
+                                self.conn.request("PATCH","/api/v0.6.3/customers/"+customer_id,json.dumps(body), headers)
 
-                    elif text[1]  == "update":
+                            else:
+                                raise ValueError('Invalid arguments')
+
+                        else:
+                            raise IOError('Invalid arguments for update!')
+
+                except e:
+                    print("Exception occured: ",e)
+
+
+            '''
+            4. Payment (to account)
+            '''
+            elif text[0] == "pay" and text[1] and text[2]:
+                try:
+                    if len(text[1])==7 and text[1].isdigit():
+                        try:
+                            amount = float(text[2])
+                        except ValueError:
+                            "Not a float"
+
+                        accounts = self.get_accounts(customer_id)
+
                         body = {
-                            text[2]:text[3]
+                          "toAccountNumber":text[1],
+                          "toSortCode":"839999",
+                          "paymentReference":"string",
+                          "paymentAmount": amount
                         }
-                        if text[2]=="mobilePhone":
-                            conn.request("PATCH", "/api/v0.6.3/customers/"+customer_id,"",headers)
-                            response = conn.getresponse()
-                            data = response.read()
-                            print(data)
+                        flag = 0
+                        for acc in accounts:
+                            conn.request("POST", "/api/v0.6.3/accounts/"+acc["id"]+"/payments", json.dumps(body), headers)
+                            response = conn.getresponse
 
-
-            elif text[0] == "list":
-
-                if text[1] == "all":
+                            if str(response.status) == "200":
+                                flag = 1
+                                break
+                        if flag==0:
+                            raise SystemError('Cannot complete transaction!')
+                except e:
+                    print("Exception occured: ",e)
                 #all atms
 
-                if text[1] == "near":
+            '''
+            5. Branches nearby
+            '''
+            elif text[0] == "branch" and text[1]=="near":
                 # ask for lat and long or extract location from twitter
 
-            elif text[0] == "branches":
-                if text[1] == "all":
-                #all branches
-
-                if text[1] == "near":
-                # ask for lat and long or extract location from twitter
+            '''
+            6. ATMs nearby
+            '''
+            elif text[0] == "atm" and text[1]=="near":
 
 
-
-
+            '''
+            Invalid
+            '''
+            else:
+                print("Unknown commands, please try again.")
 
         return True
 
